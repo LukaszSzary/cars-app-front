f006355a824cb563146f1f950fe407123e1a05a4
Łukasz Szary <lukasz55szary@gmail.com>
2023-10-28 12:32:48 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Car } from './Car';
+import { useState } from 'react';
 
 function App() {
+  const [Cars, setCars] = useState<Car[]>([{
+    id: "1993d559-96a7-437b-b36b-11b204d2b3b0",
+    brand: "Renault",
+    model: "Clio II",
+    doorsNumber: 5,
+    luggageCapacity: 300,
+    engineCapacity: 1149,
+    fuelType: "f",
+    productionDate: "2005-07-19T07:39:22.8536518",
+    carFuelConsumption: 7.2,
+    bodyType: "f"
+  },
+  {
+    id: "1993d559-96a7-437b-b36b-11b204d2b3b0",
+    brand: "Renault",
+    model: "Clio II",
+    doorsNumber: 5,
+    luggageCapacity: 300,
+    engineCapacity: 1149,
+    fuelType: "f",
+    productionDate: "2005-07-19T07:39:22.8536518",
+    carFuelConsumption: 7.2,
+    bodyType: "f"
+  }]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ddd
       </header>
+      <ul>
+        {Cars.map(car => (<li key={car.id}>
+          <div>{car.productionDate} {car.brand}</div>
+        </li>))}
+
+      </ul>
     </div>
   );
 }
---
+++ src/Car.tsx
@@ -0,0 +1,12 @@
+export interface Car{
+    id : string,
+    brand: string,
+    model: string,
+    doorsNumber: number,
+    luggageCapacity: number,
+    engineCapacity: number,
+    fuelType : string,
+    productionDate: string,
+    carFuelConsumption : number,
+    bodyType : string
+}
